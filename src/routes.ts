6ad7766b9b3a5de73b952f50046335bfb8255a97
pringon <gojedan98@yahoo.com>
2019-02-10 17:09:58 +0000

    Improve readability in routes.js

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,14 @@ import ShipStore from './store/ShipStore';
 
 const routes = (app: Application) => {
 
+  // Route for game to access user input.
   app.route('/metrics')
-  .get((req: Request, res: Response) => {
-    ShipStore.getShipMetrics()
-    .then(metrics => res.json(metrics))
-    .catch(err => res.json(err));
-  });
+    .get((req: Request, res: Response) => {
+
+      ShipStore.getShipMetrics()
+      .then(metrics => res.json(metrics))
+      .catch(err => res.json(err));
+    });
 };
 
 export default routes;
