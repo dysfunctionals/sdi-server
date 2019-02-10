ca771f4d5635e1e51bc3e4151fe8f11756ca7c95
pringon <gojedan98@yahoo.com>
2019-02-10 08:49:14 +0000

    Add logging for client connection

--- src/sockets/ClientSocket.ts
+++ src/sockets/ClientSocket.ts
@@ -20,6 +20,8 @@ export default class ClientSocket extends Socket {
         PlayerStore.assignRole(socket.id.replace('/client#', ''))
         .then((encodedRole: string) => {
           const decodedRole: string[] = encodedRole.split('-');
+          // tslint:disable-next-line
+          console.log(`User ${socket.id.replace('/client#', '')} connected to ship ${decodedRole[0]} and role ${decodedRole[1]}`);
           socket.emit('ROLE_SELECTED', JSON.stringify({
             ship: decodedRole[0],
             role: decodedRole[1],
@@ -33,11 +35,14 @@ export default class ClientSocket extends Socket {
 
       socket.on('POWER_CHANGED', (encodedData) => {
         const { ship, ...distribution } = encodedData;
+        // tslint:disable-next-line
+        console.log(`Ship ${ship} power distributions is now ${distribution.join(',')}`);
         ShipStore.changePowerDistribution(ship, distribution);
       });
 
       socket.on('ANGLE_CHANGED', (encodedData) => {
         const { ship, role, angle, on: active } = encodedData;
+        console.log(`${role} of ship ${ship} is pointed at ${angle}`);
         ShipStore.changeAngle(ship, role, angle, active);
       });
 
